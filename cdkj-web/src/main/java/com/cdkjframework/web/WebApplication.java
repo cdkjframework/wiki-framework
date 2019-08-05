cd7b39beacd2ea29bd023174c9b03da068a70ba6
卢布白菜 <jpst@vip.qq.com>
2019-08-05 18:01:32 +0800

    修改mq

---
+++ cdkj-web/src/main/java/com/cdkjframework/web/WebApplication.java
@@ -0,0 +1,36 @@
+package com.cdkjframework.web;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * @ProjectName: cdkj-framework
+ * @Package: com.cdkjframework.web
+ * @ClassName: WebApplaction
+ * @Description: java类作用描述
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class
+})
+@EnableApolloConfig
+public class WebApplication {
+
+    /**
+     * 启动方法
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
