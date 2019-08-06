8acbcc7b4074ffa94d54e3f28552a2053b645146
卢布白菜 <jpst@vip.qq.com>
2019-08-06 09:32:43 +0800

    修改mq

--- cdkj-entity/src/main/java/com/cdkjframework/entity/pay/PayConfigEntity.java
+++ cdkj-entity/src/main/java/com/cdkjframework/entity/pay/PayConfigEntity.java
@@ -21,19 +21,79 @@ import java.sql.Timestamp;
 @Setter
 @ToString
 public class PayConfigEntity {
+
+    /**
+     * 主键
+     */
     private String id;
+
+    /**
+     * 主键
+     */
     private String appId;
+
+    /**
+     * 主键
+     */
     private String secretKey;
+
+    /**
+     * 主键
+     */
     private String mchId;
+
+    /**
+     * 主键
+     */
     private String payType;
+
+    /**
+     * 主键
+     */
     private String publicKey;
+
+    /**
+     * 主键
+     */
     private String privateKey;
+
+    /**
+     * 主键
+     */
     private Timestamp addTime;
+
+    /**
+     * 主键
+     */
     private String remarks;
+
+    /**
+     * 主键
+     */
     private String notifyUrl;
+
+    /**
+     * 主键
+     */
     private int failureTime;
+
+    /**
+     * 主键
+     */
     private String apiAddress;
+
+    /**
+     * 主键
+     */
     private String queryAddress;
+
+    /**
+     * 主键
+     */
     private BigDecimal payAmount;
+
+    /**
+     * 主键
+     */
     private BigDecimal unitPrice;
 }
