d43b880a9716893af29c831e4a0d308558072917
卢布白菜 <jpst@vip.qq.com>
2019-09-27 11:08:20 +0800

    增加邮件发送

---
+++ cdkj-util/src/main/java/com/cdkjframework/config/MailConfig.java
@@ -0,0 +1,69 @@
+package com.cdkjframework.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: cdkj-framework
+ * @Package: com.cdkjframework.config
+ * @ClassName: MailConfig
+ * @Description: java类作用描述
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class MailConfig {
+
+    /**
+     * 主机
+     */
+    private String host;
+
+    /**
+     * 端口
+     */
+    private Integer port;
+
+    /**
+     * 账号
+     */
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 发邮件地址
+     */
+    private String fromMail;
+
+    /**
+     * 协议
+     */
+    private String protocol;
+
+    /**
+     * 发件人名称
+     */
+    private String personal;
+
+    /**
+     * 延迟时间
+     */
+    private Integer mailTimeout = 0;
+
+    /**
+     * 编码
+     */
+    private String encoding = "UTF-8";
+}
