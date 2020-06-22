b21952e67246feb299d33a110d0136f5891454f8
frank <jpst@vip.qq.com>
2020-06-22 16:35:40 +0800

    1、增加 myBatis 读写分离数据库

---
+++ cdkj-datasource-rw/src/main/java/com/cdkjframework/datasource/rw/config/MybatisReadConfig.java
@@ -0,0 +1,54 @@
+package com.cdkjframework.datasource.rw.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: com.cdkjframework.core
+ * @Package: com.cdkjframework.core.database.mybatis.postgresql.config
+ * @ClassName: PostgreSqlConfig
+ * @Description: 读取数据库配置
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource.mybatis.read")
+public class MybatisReadConfig {
+
+    /**
+     * 数据库连接地址
+     */
+    private String url;
+
+    /**
+     * 数据库用户名
+     */
+    private String username;
+
+    /**
+     * 数据库密码
+     */
+    private String password;
+
+    /**
+     * 数据库连接驱动
+     */
+    private String driverClassName;
+
+    /**
+     * Mapper 文件路径
+     */
+    private String mybatisMapper;
+
+    /**
+     * Mapper Xml 路径
+     */
+    private String mybatisMapperXml;
+}
---
+++ cdkj-datasource-rw/src/main/java/com/cdkjframework/datasource/rw/source/DynamicDataSource.java
@@ -0,0 +1,31 @@
+package com.cdkjframework.datasource.rw.source;
+
+import com.cdkjframework.datasource.rw.holder.DynamicDataSourceHolder;
+import com.cdkjframework.enums.datasource.DynamicDataSourceGlobal;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * @ProjectName: cdkj-framework
+ * @Package: com.cdkjframework.datasource.rw.source
+ * @ClassName: DynamicDataSource
+ * @Description: java类作用描述
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    /**
+     * 返回分配的数据库的key
+     *
+     * @return 返回结果
+     */
+    @Override
+    protected Object determineCurrentLookupKey() {
+        DynamicDataSourceGlobal dynamicDataSourceGlobal = DynamicDataSourceHolder.getDataSource();
+        if (dynamicDataSourceGlobal == null || dynamicDataSourceGlobal == DynamicDataSourceGlobal.WRITE) {
+            return DynamicDataSourceGlobal.WRITE.name();
+        }
+        return DynamicDataSourceGlobal.READ.name();
+    }
+}
