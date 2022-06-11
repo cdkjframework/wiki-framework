23c8fc028fce0b844d224b56e1ec8a805e04b2fe
jpst122 <36830630+jpst122@users.noreply.github.com>
2022-06-11 23:57:38 +0800

    1、修改加密错误问题

---
+++ cdkj-util/src/main/java/com/cdkjframework/enums/AlgorithmTypeEnums.java
@@ -0,0 +1,108 @@
+package com.cdkjframework.enums;
+
+/**
+ * @ProjectName: cdkj-framework
+ * @Package: com.cdkjframework.enums
+ * @ClassName: AlgorithmTypeEnums
+ * @Description: 加密类型枚举
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+public enum AlgorithmTypeEnums implements InterfaceEnum {
+
+    /**
+     * MD5 加密
+     */
+    MD5("MD5", "MD5 加密", ""),
+
+    /**
+     * HMAC-SHA256 加密
+     */
+    HMAC_SHA256("HmacSHA256", "HMAC-SHA256 加密", ""),
+
+    /**
+     * SHA256withRSA 加密
+     */
+    SHA256withRSA("SHA256withRSA", "SHA256withRSA 加密", ""),
+
+    /**
+     * HMAC-SHA384 加密
+     */
+    HMAC_SHA384("HMAC-SHA-384", "HMAC-SHA384 加密", ""),
+
+    /**
+     * HMAC-SHA512 加密
+     */
+    HMAC_SHA512("HMAC-SHA-512", "HMAC-SHA512 加密", ""),
+
+    /**
+     * RSA2 加密
+     */
+    RSA2("RSA2", "RSA2 加密", ""),
+
+    /**
+     * RSA 加密
+     */
+    RSA("RSA", "RSA 加密", ""),
+
+    /**
+     * SHA256 加密
+     */
+    SHA256("SHA-256", "SHA256 加密", ""),
+
+    /**
+     * RSA 加密
+     */
+    SHA384("SHA-384", "SHA384 加密", ""),
+
+    /**
+     * SHA512 加密
+     */
+    SHA512("SHA-512", "SHA512 加密", "");
+
+    private String value, text, node;
+
+    /**
+     * 构造函数
+     *
+     * @param value 值
+     * @param text  说明
+     * @param node  未使用
+     */
+    AlgorithmTypeEnums(String value, String text, String node) {
+        this.value = value;
+        this.text = text;
+        this.node = node;
+    }
+
+
+    /**
+     * 获取值
+     *
+     * @return 返回值
+     */
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * 获取描述
+     *
+     * @return 返描述
+     */
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * 获取下节点值
+     *
+     * @return 返下节点值
+     */
+    @Override
+    public String getNode() {
+        return node;
+    }
+}
--- cdkj-util/src/main/java/com/cdkjframework/util/encrypts/Md5Utils.java
+++ cdkj-util/src/main/java/com/cdkjframework/util/encrypts/Md5Utils.java
@@ -1,7 +1,11 @@
 package com.cdkjframework.util.encrypts;
 
+import com.cdkjframework.constant.IntegerConsts;
+import com.cdkjframework.enums.AlgorithmTypeEnums;
 import com.cdkjframework.util.log.LogUtils;
+import com.cdkjframework.util.tool.StringUtils;
 
+import javax.crypto.Mac;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -17,11 +21,6 @@ import java.security.NoSuchAlgorithmException;
 public class Md5Utils {
 
     /**
-     * 加密类型
-     */
-    private static final String ALGORITHM = "MD5";
-
-    /**
      * 日志
      */
     private static LogUtils logUtils = LogUtils.getLogger(Md5Utils.class);
@@ -29,13 +28,103 @@ public class Md5Utils {
     /**
      * 对字符串md5加密(小写+字母)
      *
-     * @param str 传入要加密的字符串
+     * @param content 传入要加密的字符串
      * @return MD5加密后的字符串
      */
-    public static String getMd5(String str) {
+    public static String getMd5(String content) {
         try {
             // 计算md5函数
-            byte[] md5 = getMd5(str.getBytes(Charset.defaultCharset()));
+            byte[] md5 = getMd5ByByte(content.getBytes(Charset.defaultCharset()), AlgorithmTypeEnums.MD5);
+            // 转换为16进制
+            return toHexString(md5);
+        } catch (Exception e) {
+            logUtils.error(e);
+            return null;
+        }
+    }
+
+    /**
+     * 对字符串md5加密(小写+字母)
+     *
+     * @param content 传入要加密的字符串
+     * @return MD5加密后的字符串 16 位
+     */
+    public static String getMd5Hex(String content) {
+        String hex = getMd5(content);
+        if (StringUtils.isNullAndSpaceOrEmpty(hex)) {
+            return hex;
+        }
+        return hex.substring(IntegerConsts.EIGHT, IntegerConsts.TWENTY_FOUR);
+    }
+
+    /**
+     * 对字符串md5加密(小写+字母)
+     *
+     * @param data 传入要加密的 byte
+     * @return MD5加密后的字符串
+     */
+    public static String getMd5(byte[] data) {
+        try {
+            // 计算md5函数
+            byte[] md5 = getMd5ByByte(data, AlgorithmTypeEnums.MD5);
+            // 转换为16进制
+            return toHexString(md5);
+        } catch (Exception e) {
+            logUtils.error(e);
+            return null;
+        }
+    }
+
+    /**
+     * 对字符串HMAC_SHA256加密(小写+字母)
+     *
+     * @param content 传入要加密的字符串
+     * @return 加密后的字符串
+     */
+    public static String getSha256(String content) {
+        try {
+            // 计算md5函数
+            byte[] md5 = getMd5ByByte(content.getBytes(Charset.defaultCharset()),
+                    AlgorithmTypeEnums.SHA256);
+            // 转换为16进制
+            return toHexString(md5);
+        } catch (Exception e) {
+            logUtils.error(e);
+            return null;
+        }
+    }
+
+    /**
+     * 对字符串 SHA512 加密(小写+字母)
+     *
+     * @param content 传入要加密的字符串
+     * @return 加密后的字符串
+     */
+    public static String getSha512(String content) {
+        try {
+            // 计算md5函数
+            byte[] md5 = getMd5ByByte(content.getBytes(Charset.defaultCharset()),
+                    AlgorithmTypeEnums.SHA512);
+            // 转换为16进制
+            return toHexString(md5);
+        } catch (Exception e) {
+            logUtils.error(e);
+            return null;
+        }
+    }
+
+    /**
+     * 对字符串HMAC_SHA384加密(小写+字母)
+     *
+     * @param content 传入要加密的字符串
+     * @return 加密后的字符串
+     */
+    public static String getSha384(String content) {
+        try {
+            // 计算md5函数
+            byte[] md5 = getMd5ByByte(content.getBytes(Charset.defaultCharset()),
+                    AlgorithmTypeEnums.SHA384);
+            // 转换为16进制
             return toHexString(md5);
         } catch (Exception e) {
             logUtils.error(e);
@@ -49,19 +138,20 @@ public class Md5Utils {
      * @param data 字节数组
      * @return 返回加密结果
      */
-    public static byte[] getMd5(byte[] data) {
+    private static byte[] getMd5ByByte(byte[] data, AlgorithmTypeEnums autographType) {
+        byte[] bytes;
         try {
             // 生成一个MD5加密计算摘要
-            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
+            MessageDigest messageDigest = MessageDigest.getInstance(autographType.getValue());
             // 计算md5函数
             messageDigest.update(data);
-            return messageDigest.digest();
+            bytes = messageDigest.digest();
         } catch (NoSuchAlgorithmException e) {
             logUtils.error(e);
+            bytes = new byte[]{};
         }
-        return new byte[]{};
+        return bytes;
     }
-
 
     /**
      * 将加密后的字节数组，转换成16进制的字符串
@@ -72,7 +162,11 @@ public class Md5Utils {
     private static String toHexString(byte[] md5) {
         StringBuilder builder = new StringBuilder();
         for (byte by : md5) {
-            builder.append(Integer.toHexString(by & 0xff));
+            String hex = Integer.toHexString(by & 0xff);
+            if (hex.length() == IntegerConsts.ONE) {
+                builder.append(StringUtils.ZERO);
+            }
+            builder.append(hex);
         }
         // 返回结果
         return builder.toString();
