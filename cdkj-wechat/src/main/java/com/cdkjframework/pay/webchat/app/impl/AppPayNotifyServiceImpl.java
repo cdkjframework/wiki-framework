adfdafa1dfd2ab1c13a43ebe174275c264d1eede
frank-tiger <jpst@vip.qq.com>
2023-07-22 23:34:02 +0800

    1、修改websocket配置 2、修改引用版本

--- cdkj-wechat/src/main/java/com/cdkjframework/pay/webchat/app/impl/AppPayNotifyServiceImpl.java
+++ cdkj-wechat/src/main/java/com/cdkjframework/pay/webchat/app/impl/AppPayNotifyServiceImpl.java
@@ -2,7 +2,6 @@ package com.cdkjframework.pay.webchat.app.impl;
 
 import com.cdkjframework.config.WeChatConfig;
 import com.cdkjframework.constant.IntegerConsts;
-import com.cdkjframework.datasource.mongodb.repository.IMongoRepository;
 import com.cdkjframework.entity.pay.webchat.app.NotifyResultEntity;
 import com.cdkjframework.entity.pay.webchat.app.UnifiedOrderReturnEntity;
 import com.cdkjframework.exceptions.GlobalException;
@@ -13,8 +12,7 @@ import com.cdkjframework.util.log.LogUtils;
 import com.cdkjframework.util.make.GeneratedValueUtils;
 import com.cdkjframework.util.network.http.HttpServletUtils;
 import com.cdkjframework.util.tool.StringUtils;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.PrintWriter;
@@ -28,92 +26,81 @@ import java.io.PrintWriter;
  * @Version: 1.0
  */
 @Service
+@RequiredArgsConstructor
 public class AppPayNotifyServiceImpl implements AppPayNotifyService {
 
-    /**
-     * 日志
-     */
-    private LogUtils logUtils = LogUtils.getLogger(AppPayNotifyServiceImpl.class);
+  /**
+   * 日志
+   */
+  private LogUtils logUtils = LogUtils.getLogger(AppPayNotifyServiceImpl.class);
 
-    /**
-     * mongo 存储库
-     */
-    private final IMongoRepository mongoRepository;
-    /**
-     * 读取配置
-     */
-    private final WeChatConfig weChatConfig;
+  /**
+   * 读取配置
+   */
+  private final WeChatConfig weChatConfig;
 
-    /**
-     * 构造函数
-     */
-    public AppPayNotifyServiceImpl(IMongoRepository mongoRepository, WeChatConfig weChatConfig) {
-        this.mongoRepository = mongoRepository;
-        this.weChatConfig = weChatConfig;
+  /**
+   * 支付结果
+   *
+   * @param builder 返回结果
+   * @throws Exception 异常信息
+   */
+  @Override
+  public String payNotifyCallback(StringBuilder builder) throws Exception {
+    NotifyResultEntity notifyResult = XmlUtils.xmlToBean(NotifyResultEntity.class, builder.toString());
+
+    // 封装结果
+    UnifiedOrderReturnEntity orderReturn = new UnifiedOrderReturnEntity();
+    try {
+      if (RESULTS_SUCCESS.equals(notifyResult.getResultCode())) {
+        checkSignature(notifyResult);
+      }
+      orderReturn.setReturnCode(RESULTS_SUCCESS);
+      orderReturn.setReturnMsg(RESULTS_MSG);
+    } catch (GlobalException e) {
+      orderReturn.setReturnCode(RESULTS_FAIL);
+      orderReturn.setReturnMsg(e.getMessage());
     }
 
-    /**
-     * 支付结果
-     *
-     * @param builder 返回结果
-     * @throws Exception 异常信息
-     */
-    @Override
-    public String payNotifyCallback(StringBuilder builder) throws Exception {
-        NotifyResultEntity notifyResult = XmlUtils.xmlToBean(NotifyResultEntity.class, builder.toString());
+    // 返回输出结果
+    PrintWriter writer = HttpServletUtils.getResponse().getWriter();
+    writer.write(XmlUtils.beanToXml(UnifiedOrderReturnEntity.class, orderReturn));
 
-        // 封装结果
-        UnifiedOrderReturnEntity orderReturn = new UnifiedOrderReturnEntity();
-        try {
-            if (RESULTS_SUCCESS.equals(notifyResult.getResultCode())) {
-                checkSignature(notifyResult);
-            }
-            orderReturn.setReturnCode(RESULTS_SUCCESS);
-            orderReturn.setReturnMsg(RESULTS_MSG);
-        } catch (GlobalException e) {
-            orderReturn.setReturnCode(RESULTS_FAIL);
-            orderReturn.setReturnMsg(e.getMessage());
-        }
+    // 返回订单
+    if (RESULTS_FAIL.equals(orderReturn.getReturnCode())) {
+      return StringUtils.Empty;
+    } else {
+      return notifyResult.getOutTradeNo();
+    }
+  }
 
-        // 返回输出结果
-        PrintWriter writer = HttpServletUtils.getResponse().getWriter();
-        writer.write(XmlUtils.beanToXml(UnifiedOrderReturnEntity.class, orderReturn));
-
-        // 返回订单
-        if (RESULTS_FAIL.equals(orderReturn.getReturnCode())) {
-            return StringUtils.Empty;
-        } else {
-            return notifyResult.getOutTradeNo();
-        }
+  /**
+   * 验证签名
+   *
+   * @param notifyResult 数据结果
+   * @throws GlobalException 异常信息
+   */
+  public void checkSignature(NotifyResultEntity notifyResult) throws GlobalException {
+    // 查询条件
+//    Query query = new Query();
+//    Criteria criteria = Criteria.where("outTradeNo").is(notifyResult.getOutTradeNo());
+//    criteria.and("nonceStr").is(notifyResult.getNonceStr());
+    UnifiedOrderReturnEntity orderReturn = null;//mongoRepository.findEntity(query, UnifiedOrderReturnEntity.class);
+    orderReturn.setStatus(IntegerConsts.ONE);
+    // 效验签名
+    String signStr = String.format("appid=%s&body=%s&device_info=%s&mch_id=%s&nonce_str=%s&key=%s",
+        weChatConfig.getAppId(), orderReturn.getBody(), orderReturn.getDeviceInfo(), weChatConfig.getMchId(),
+        orderReturn.getNonceStr(), weChatConfig.getSecretKey());
+    String sign = Md5Utils.getMd5(signStr);
+    if (!sign.equals(notifyResult.getSign())) {
+      throw new GlobalException("签名错误！");
     }
 
-    /**
-     * 验证签名
-     *
-     * @param notifyResult 数据结果
-     * @throws GlobalException 异常信息
-     */
-    public void checkSignature(NotifyResultEntity notifyResult) throws GlobalException {
-        // 查询条件
-        Query query = new Query();
-        Criteria criteria = Criteria.where("outTradeNo").is(notifyResult.getOutTradeNo());
-        criteria.and("nonceStr").is(notifyResult.getNonceStr());
-        UnifiedOrderReturnEntity orderReturn = mongoRepository.findEntity(query, UnifiedOrderReturnEntity.class);
-        orderReturn.setStatus(IntegerConsts.ONE);
-        // 效验签名
-        String signStr = String.format("appid=%s&body=%s&device_info=%s&mch_id=%s&nonce_str=%s&key=%s",
-                weChatConfig.getAppId(), orderReturn.getBody(), orderReturn.getDeviceInfo(), weChatConfig.getMchId(),
-                orderReturn.getNonceStr(), weChatConfig.getSecretKey());
-        String sign = Md5Utils.getMd5(signStr);
-        if (!sign.equals(notifyResult.getSign())) {
-            throw new GlobalException("签名错误！");
-        }
-
-        // 保存执行结果
-        mongoRepository.save(orderReturn);
-        // 保存返回结果
-        notifyResult.setId(GeneratedValueUtils.getOrderlyShortUuid());
-        notifyResult.setOrderId(orderReturn.getId());
-        mongoRepository.save(notifyResult);
-    }
+    // 保存执行结果
+//        mongoRepository.save(orderReturn);
+    // 保存返回结果
+    notifyResult.setId(GeneratedValueUtils.getOrderlyShortUuid());
+    notifyResult.setOrderId(orderReturn.getId());
+//    mongoRepository.save(notifyResult);
+  }
 }
