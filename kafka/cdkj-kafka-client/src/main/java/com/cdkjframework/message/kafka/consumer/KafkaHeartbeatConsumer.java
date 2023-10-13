d9e8faa76c39ca972f505de6f32a82a4e91c5ff7
jpst122 <jpst@vip.qq.com>
2023-10-13 09:32:49 +0800

    1、kafka配置

---
+++ kafka/cdkj-kafka-client/src/main/java/com/cdkjframework/message/kafka/consumer/KafkaHeartbeatConsumer.java
@@ -0,0 +1,12 @@
+package com.cdkjframework.message.kafka.consumer;
+
+/**
+ * @ProjectName: cdkj-framework
+ * @Package: com.cdkjframework.message.queue.kafka.consumer
+ * @ClassName: com.cdkjframework.message.kafka.consumer.KafkaHeartbeatConsumer
+ * @Description: java类作用描述
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+public class KafkaHeartbeatConsumer {
+}
