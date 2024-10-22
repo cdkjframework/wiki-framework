5d7de67e8bf78506dd3bbbbdc51df96857b4e6b5
jpst122 <jpst@vip.qq.com>
2024-10-22 16:07:09 +0800

    1、修改BUG

--- queue/cdkj-kafka/src/main/java/com/cdkjframework/kafka/producer/ProducerConfiguration.java
+++ queue/cdkj-kafka/src/main/java/com/cdkjframework/kafka/producer/ProducerConfiguration.java
@@ -2,7 +2,6 @@ package com.cdkjframework.kafka.producer;
 
 import com.cdkjframework.constant.IntegerConsts;
 import com.cdkjframework.kafka.producer.config.KafkaConfig;
-import com.cdkjframework.kafka.producer.util.ProducerUtils;
 import com.cdkjframework.util.tool.StringUtils;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.CommonClientConfigs;
@@ -16,7 +15,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import jakarta.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -62,27 +60,29 @@ public class ProducerConfiguration {
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
 
     // 重试次数，0为不启用重试机制
-    props.put(ProducerConfig.RETRIES_CONFIG, kafkaConfig.getRetries());
-    //同步到副本, 默认为1
-    // acks=0 把消息发送到kafka就认为发送成功
-    // acks=1 把消息发送到kafka leader分区，并且写入磁盘就认为发送成功
-    // acks=all 把消息发送到kafka leader分区，并且leader分区的副本follower对消息进行了同步就任务发送成功
-    props.put(ProducerConfig.ACKS_CONFIG, kafkaConfig.getAcks());
+		props.put(ProducerConfig.RETRIES_CONFIG, kafkaConfig.getRetries());
+		//同步到副本, 默认为1
+		// acks=0 把消息发送到kafka就认为发送成功
+		// acks=1 把消息发送到kafka leader分区，并且写入磁盘就认为发送成功
+		// acks=all 把消息发送到kafka leader分区，并且leader分区的副本follower对消息进行了同步就任务发送成功
+		props.put(ProducerConfig.ACKS_CONFIG, kafkaConfig.getAcks());
 
-    // 生产者空间不足时，send()被阻塞的时间，默认60s
-    props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, kafkaConfig.getMaxBlock());
-    // security.providers
-    props.put(ProducerConfig.SECURITY_PROVIDERS_CONFIG, kafkaConfig.getSecurityProviders());
-    // 控制批处理大小，单位为字节
-    props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaConfig.getBatchSize());
-    // 批量发送，延迟为1毫秒，启用该功能能有效减少生产者发送消息次数，从而提高并发量
-    props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaConfig.getLinger());
-    // 生产者可以使用的总内存字节来缓冲等待发送到服务器的记录
-    props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, kafkaConfig.getBufferMemory());
-    // 消息的最大大小限制,也就是说send的消息大小不能超过这个限制, 默认1048576(1MB)
-    props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, kafkaConfig.getMaxRequestSize());
-    // 键的序列化方式
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		// 生产者空间不足时，send()被阻塞的时间，默认60s
+		props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, kafkaConfig.getMaxBlock());
+		// security.providers
+		if (StringUtils.isNotNullAndEmpty(kafkaConfig.getSecurityProviders())) {
+			props.put(ProducerConfig.SECURITY_PROVIDERS_CONFIG, kafkaConfig.getSecurityProviders());
+		}
+		// 控制批处理大小，单位为字节
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaConfig.getBatchSize());
+		// 批量发送，延迟为1毫秒，启用该功能能有效减少生产者发送消息次数，从而提高并发量
+		props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaConfig.getLinger());
+		// 生产者可以使用的总内存字节来缓冲等待发送到服务器的记录
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, kafkaConfig.getBufferMemory());
+		// 消息的最大大小限制,也就是说send的消息大小不能超过这个限制, 默认1048576(1MB)
+		props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, kafkaConfig.getMaxRequestSize());
+		// 键的序列化方式
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     // 值的序列化方式
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     // 压缩消息，支持四种类型，分别为：none、lz4、gzip、snappy，默认为none。
