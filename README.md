e16f1e5b148a4acb96a0755e74b41984ce1bdf86
维基框架 <jpst@vip.qq.com>
2025-07-02 19:08:17 +0800

    1、修改获取地址BUG 2、修改本地址说明

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
-# cdkjFramework（维基框架）
+<p align="center">
+	<a href="https://framewiki.com"><img alt="keytool" src="assets/wiki.png"/></a>
+</p>
 
-#### 介绍
-cdkjFw 开发框架基础公共包，方便快捷分模块引用。三方包统一管理
-框架官网 [https://www.framewiki.com](https://www.framewiki.com)
+<p align="center">
+	<a href="https://framewiki.com">https://framewiki.com/</a>
+</p>
 
-#### 软件架构
-基于Spring Boot 3.3.5 及 Spring Cloud 4.1.4，MyBatis、JPA、PageHelper（分页插件）、alibaba数据库驱动
+<p align="center">
+  <a href='https://gitee.com/cdkjframework/wiki-proxy/stargazers'><img src='https://gitee.com/cdkjframework/wiki-proxy/badge/star.svg?theme=dark' alt='star'></img></a>
+<a href='https://gitee.com/cdkjframework/wiki-proxy/members'><img src='https://gitee.com/cdkjframework/wiki-proxy/badge/fork.svg?theme=dark' alt='fork'></img></a>
+<a target="_blank" href="https://www.oracle.com/java/technologies/javase/jdk17-0-13-later-archive-downloads.html">
+    <img src="https://img.shields.io/badge/JDK-17+-red.svg" />
+</a>
+<a href="./LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-red" alt="license MIT">
+</a>
+</p>
 
-包含以下工具：JWT、Redis、easyPoi导入导出excel、mongodb（基于Spring Boot）、webSocket（基于netty）、汉字转拼音、swagger2（接口管理）、AliYun OSS、AliYun 短信、fasterxml.jackson、zxing、mqttv3、kryo、cxf Web Service及密码加密码（或解密）、GZIP压缩、HTTP请求等
+## 介绍
 
-#### 安装教程
+Wiki-Framework 为开发者而生、；是一个功能丰富且易用的Java工具库，通过诸多实用工具类的使用，旨在帮助开发为快速搭建开发项目提供便利。
+这些封装的工具涵盖了字符串、数字、集合、编码、日期、文件、IO、加密、数据库JDBC、JSON、HTTP、TCP（WebSocket、Socket）客户端等一系列操作，可以满足各种不同场景的开发需求。
+
+## 软件架构
+
+基于Spring Boot 3.3.5 及 Spring Cloud 4.1.4，MyBatis、JPA、PageHelper（分页插件）、Alibaba数据库驱动
+
+包含以下工具：JWT、Redis、easyPoi导入导出excel、mongodb（基于Spring Boot）、webSocket（基于netty）、汉字转拼音、swagger2（接口管理）、AliYun
+OSS、AliYun 短信、fasterxml.jackson、zxing、mqttv3、kryo、cxf Web Service及密码加密码（或解密）、GZIP压缩、HTTP请求等
+
+## 包含组件
+
+一个Java基础工具类，对文件、流、加密解密、转码、正则、线程、XML等JDK方法进行封装，组成各种Utils工具类，同时提供以下组件：
+
+| 模块        | 介绍                                                     |
+|-----------|--------------------------------------------------------|
+| wiki-core | 核心工具模块，提供项目工程启动的必要配置，公共请求响应拦截器（实现参数AES加觖密处理），公共异常捕获，处理 |
+
+## 安装教程
 
 不需要安装直接使用
 
@@ -23,7 +51,7 @@ cdkjFw 开发框架基础公共包，方便快捷分模块引用。三方包统�
     <parent>
         <groupId>com.framewiki</groupId>
         <artifactId>wiki-pom</artifactId>
-        <version>1.0.6</version>
+        <version>1.0.11</version>
     </parent>
 
 #### 参与贡献
@@ -31,6 +59,7 @@ cdkjFw 开发框架基础公共包，方便快捷分模块引用。三方包统�
 卢布白菜
 
 #### 使用公司
+
 注：排名根据登记序列
 
 1、宏图物流股份有限公司
--- wiki-util/src/main/java/com/cdkjframework/util/network/http/HttpServletUtils.java
+++ wiki-util/src/main/java/com/cdkjframework/util/network/http/HttpServletUtils.java
@@ -19,93 +19,98 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 public class HttpServletUtils {
 
-    /**
-     * IP头部变量
-     */
-    private static final String HEADER_IP = "X-Real-IP";
+  /**
+   * IP头部变量
+   */
+  private static final String HEADER_IP = "X-Real-IP";
 
-    /**
-     * HttpServletRequest
-     *
-     * @return 返回结果
-     */
-    public static HttpServletRequest getContext() {
-        try {
-            ServletRequestAttributes attributes = getRequestAttributes();
-            if (attributes == null) {
-                return null;
-            } else {
-                return attributes.getRequest();
-            }
-        } catch (Exception e) {
-            return null;
-        }
+  /**
+   * HttpServletRequest
+   *
+   * @return 返回结果
+   */
+  public static HttpServletRequest getContext() {
+    try {
+      ServletRequestAttributes attributes = getRequestAttributes();
+      if (attributes == null) {
+        return null;
+      } else {
+        return attributes.getRequest();
+      }
+    } catch (Exception e) {
+      return null;
     }
+  }
 
-    /**
-     * HttpServletRequest
-     *
-     * @return 返回结果
-     */
-    public static HttpServletRequest getRequest() {
-        try {
-            ServletRequestAttributes attributes = getRequestAttributes();
-            if (attributes == null) {
-                return null;
-            } else {
-                return attributes.getRequest();
-            }
-        } catch (Exception e) {
-            return null;
-        }
+  /**
+   * HttpServletRequest
+   *
+   * @return 返回结果
+   */
+  public static HttpServletRequest getRequest() {
+    try {
+      ServletRequestAttributes attributes = getRequestAttributes();
+      if (attributes == null) {
+        return null;
+      } else {
+        return attributes.getRequest();
+      }
+    } catch (Exception e) {
+      return null;
     }
-    /**
-     * HttpServletResponse
-     *
-     * @return 返回结果
-     */
-    public static HttpServletResponse getResponse() {
-        ServletRequestAttributes attributes = getRequestAttributes();
-        if (attributes == null) {
-            return null;
-        } else {
-            return attributes.getResponse();
-        }
-    }
+  }
 
-    /**
-     * 请求属性
-     *
-     * @return 返回结果
-     */
-    private static ServletRequestAttributes getRequestAttributes() {
-        RequestAttributes attributes = RequestContextHolder.getRequestAttributes() == null ?
-                RequestContextHolder.currentRequestAttributes() : RequestContextHolder.getRequestAttributes();
-        if (attributes == null) {
-            return null;
-        }
-        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) attributes;
-        try {
-            //设置子线程共享
-            RequestContextHolder.setRequestAttributes(servletRequestAttributes, true);
-        } catch (Exception e) {
-
-        }
-        return servletRequestAttributes;
+  /**
+   * HttpServletResponse
+   *
+   * @return 返回结果
+   */
+  public static HttpServletResponse getResponse() {
+    ServletRequestAttributes attributes = getRequestAttributes();
+    if (attributes == null) {
+      return null;
+    } else {
+      return attributes.getResponse();
     }
+  }
 
-    /**
-     * 获取客户端IP地址
-     *
-     * @return 返回IP地址
-     */
-    public static String getRemoteAddr() {
-        //
-        String localAddr = getRequest().getHeader(HEADER_IP);
-        if (StringUtils.isNullAndSpaceOrEmpty(localAddr)) {
-            localAddr = getRequest().getRemoteAddr();
-        }
-        // 返回结果
-        return localAddr;
+  /**
+   * 请求属性
+   *
+   * @return 返回结果
+   */
+  private static ServletRequestAttributes getRequestAttributes() {
+    RequestAttributes attributes = RequestContextHolder.getRequestAttributes() == null ?
+        RequestContextHolder.currentRequestAttributes() : RequestContextHolder.getRequestAttributes();
+    if (attributes == null) {
+      return null;
     }
+    ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) attributes;
+    try {
+      //设置子线程共享
+      RequestContextHolder.setRequestAttributes(servletRequestAttributes, true);
+    } catch (Exception e) {
+
+    }
+    return servletRequestAttributes;
+  }
+
+  /**
+   * 获取客户端IP地址
+   *
+   * @return 返回IP地址
+   */
+  public static String getRemoteAddr() {
+    HttpServletRequest request = getRequest();
+    if (request == null) {
+      return StringUtils.EMPTY;
+    }
+    // 获取地址
+    String localAddr = request.getHeader(HEADER_IP);
+    if (StringUtils.isNullAndSpaceOrEmpty(localAddr)) {
+      localAddr = request.getRemoteAddr();
+    }
+    // 返回结果
+    return localAddr;
+  }
 }
