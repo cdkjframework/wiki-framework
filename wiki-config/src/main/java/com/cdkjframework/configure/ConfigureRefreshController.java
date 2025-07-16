eb90a1b75227512dc545e3aa5ae3b6687d345790
维基框架 <jpst@vip.qq.com>
2025-07-16 15:50:10 +0800

    1、删除说明

--- wiki-config/src/main/java/com/cdkjframework/configure/ConfigureRefreshController.java
+++ wiki-config/src/main/java/com/cdkjframework/configure/ConfigureRefreshController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigureRefreshController {
 
     /**
-     * 上下文刷新器 ZHONGYIDONG
+     * 上下文刷新器
      */
     private ContextRefresher contextRefresher;
 
--- wiki-config/src/main/java/com/cdkjframework/configure/package-info.java
+++ wiki-config/src/main/java/com/cdkjframework/configure/package-info.java
@@ -1,5 +1,23 @@
 /**
- * Spring Cloud Config 客户端配置读取及刷新接口
+ * <p>Spring Cloud Config 客户端配置读取及刷新接口</p>
+ *
+ * 需要在配置文件中配置：
+ * management:
+ *   endpoints:
+ *     web:
+ *       exposure:
+ *         include: refresh,bus-refresh  # 暴露 /actuator/refresh 若项目整体在内网可考虑配置为
+ *         若要自动批量刷新（通过 RabbitMQ 或 Kafka 传递消息） 暴露 Bus-Refresh 端点，还需要引入
+ * RabbitMQ：
+ * <dependency>
+ *     <groupId>org.springframework.cloud</groupId>
+ *     <artifactId>spring-cloud-starter-bus-amqp</artifactId>
+ * </dependency>
+ * Kafka：
+ * <dependency>
+ *     <groupId>org.springframework.cloud</groupId>
+ *     <artifactId>spring-cloud-starter-bus-kafka</artifactId>
+ * </dependency>
  *
  * @ProjectName: wiki-framework
  * @Package: com.cdkjframework.configure
